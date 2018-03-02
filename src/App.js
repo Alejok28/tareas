34ac0814ac127a04de44d79419888cf6b515c3e3
Alejok28 <alejo_k28@hotmail.com>
2018-03-02 12:05:45 -0500

    Push input value into list

--- src/App.js
+++ src/App.js
@@ -3,18 +3,38 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      term: '',
+      tasks: ['Sacar la ropa', 'Hacer la cama', 'Leer un rato']
+    }
+  }
+
+  handleChange(e) {
+    this.setState({
+      term: e.target.value
+    })
+  }
+
+  handelSubmit(e) {
+    e.preventDefault()
+    this.setState({
+      term: '',
+      tasks: [...this.state.tasks, this.state.term]
+    })
+  }
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {this.state.tasks.map((task, id) => <li key= {id}>{task}</li>)}
           </ul>
-           <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+           <form onSubmit={this.handelSubmit.bind(this)}>
+             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" onChange={this.handleChange.bind(this)} value={this.state.term}/>
            </form>
         </div>
       </div>
